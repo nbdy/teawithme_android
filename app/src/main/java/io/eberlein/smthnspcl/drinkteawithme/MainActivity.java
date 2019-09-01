9dfd35c269d770d8165d3c1693c90cf7ae4ac13a
trig0n <pascal@eberlein.io>
2019-09-01 12:30:16 +0200

    update

--- app/src/main/java/io/eberlein/smthnspcl/drinkteawithme/MainActivity.java
+++ app/src/main/java/io/eberlein/smthnspcl/drinkteawithme/MainActivity.java
@@ -3,6 +3,7 @@ package io.eberlein.smthnspcl.drinkteawithme;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,16 +20,17 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity
@@ -42,6 +44,16 @@ public class MainActivity extends AppCompatActivity
     DrawerLayout drawer;
     @BindView(R.id.nav_view)
     NavigationView navigationView;
+
+    @OnClick(R.id.logout)
+    public void logout() {
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                    }
+                });
+    }
 
     private FragmentManager fragmentManager;
 
@@ -128,6 +140,7 @@ public class MainActivity extends AppCompatActivity
                     // Get an instance of AuthUI based on the default app
                     AuthUI.getInstance()
                             .createSignInIntentBuilder()
+                            .setTheme(R.style.BlackTheme)
                             .setAvailableProviders(Arrays.asList(
                                     new AuthUI.IdpConfig.EmailBuilder().build()
                             ))
